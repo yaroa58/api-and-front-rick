81e9b05fd580df982bb22b33597914e416f275bb
Yeison Roa <alexandr.ortega@hotmail.com>
2022-11-25 00:31:52 -0500

    despliege api rick

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
+import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar'
+import Characters from './components/Characters';
+import Pagination from './components/Pagination';
 
 function App() {
+
+  const [characters, setCharacters] = useState([]);
+  const [info, setInfo] = useState({});
+
+
+  const initialUrl = "https://rickandmortyapi.com/api/character";
+  const fetchCharacters = (url) => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCharacters(data.results);
+        setInfo(data.info);
+      })
+      .catch(error => console.log(error))
+
+  };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev)
+  }
+
+
+  const onNext = () => {
+
+    fetchCharacters(info.next);
+  }
+
+  useEffect(() => {
+    fetchCharacters(initialUrl);
+  }, [])
   return (
-    <Navbar brand="Rick And Morty App" />
+    <>
+      <Navbar brand="Rick And Morty App" />
+      <div className='container mt-5'>
+        <Pagination prev={info.prev} next={info.next}
+          onPrevious={onPrevious}
+          onNext={onNext} />
+        <Characters characters={characters} />
+        <Pagination />
+      </div>
+    </>
   );
 }
 
